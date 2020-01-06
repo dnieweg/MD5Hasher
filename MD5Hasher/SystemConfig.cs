07fbb0078b66f581d7cabd1c71f2e0342720b731
Daniel Nieweglowski <dn00gs@outlook.com>
2020-01-06 01:53:06 -0600

    Created Configuration folder and yaml file

---
+++ MD5Hasher/SystemConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MD5Hasher
+{
+    class SystemConfig
+    {
+        private string directoryPath;
+        private string currentWorkingDir;
+
+        public String getDirectoryPath()
+        {
+            return directoryPath;
+        }
+        public void setDirectoryPath(string path)
+        {
+            directoryPath = path;
+        }
+
+        public String getCWDPath()
+        {
+            return currentWorkingDir;
+        }
+        public void setCWDPath(string path)
+        {
+            currentWorkingDir = path;
+        }
+    }
+}
